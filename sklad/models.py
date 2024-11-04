82efd4bcfe38914b3dfa519a090d0c68dfd792f5
AjiniyazReymov <rajiniyaz5@gmail.com>
2024-11-04 22:51:19 +0500

    Update models.py

--- sklad/models.py
+++ sklad/models.py
@@ -1,9 +1,13 @@
+from django.utils import timezone
+
 from django.db import models
-from rest_framework.response import Response
+
 
 
 class Material(models.Model):
     name = models.CharField(max_length=100)
+    created_time = models.DateTimeField(default=timezone.now)
+    updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -12,6 +16,8 @@ class Warehouse(models.Model):
     material_id = models.ForeignKey(Material, on_delete=models.CASCADE)
     remainder = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=30, decimal_places=2)
+    created_time = models.DateTimeField(default=timezone.now,)
+    updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.material_id}"
@@ -19,22 +25,18 @@ class Warehouse(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=6, blank=True, unique=True)
+    created_time = models.DateTimeField(default=timezone.now,)
+    updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
 
 class ProductMaterial(models.Model):
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    material_id = models.ForeignKey(Material, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    material_id = models.ForeignKey(Material, on_delete=models.CASCADE, related_name='materials')
     qty = models.IntegerField()
+    created_time = models.DateTimeField(default=timezone.now,)
+    updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.product_id}"
-
-class Shirt(ProductMaterial, models.Model):
-    fabric_required = models.FloatField(default=0.8)  # м ткани на 1 рубашку
-    buttons_required = models.IntegerField(default=5)  # количество пуговиц на 1 рубашку
-    thread_required = models.FloatField(default=10)  # м ниток на 1 рубашку
-
-    def __str__(self):
-        return f"Shirt requirements: {self.fabric_required}m fabric, {self.buttons_required} buttons, {self.thread_required}m thread"
